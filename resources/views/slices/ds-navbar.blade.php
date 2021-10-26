5cd8cf48c660c56ffaa556a30426bda35cfe979f
Kacong Arip <arifsaqa@gmail.com>
2021-10-26 21:34:33 +0700

    s

--- resources/views/slices/ds-navbar.blade.php
+++ resources/views/slices/ds-navbar.blade.php
@@ -13,7 +13,7 @@
       </a>
       <div class="dropdown-menu dropdown-menu-right">
         <!-- <div class="dropdown-title" id="">Logged in 5 min ago</div> -->
-        <a href="features-profile.html" class="dropdown-item has-icon">
+        <a href="#" class="dropdown-item has-icon">
           <i class="far fa-user"></i> Profile
         </a>
         <div class="dropdown-divider"></div>
