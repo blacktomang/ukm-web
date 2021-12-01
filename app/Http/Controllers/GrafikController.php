d0eeb6dc332182a3cdb2394c5b05d5cc337a4292
Kacong Arip <arifsaqa@gmail.com>
2021-12-01 08:52:01 +0700

    include all store

--- app/Http/Controllers/GrafikController.php
+++ app/Http/Controllers/GrafikController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,20 +11,29 @@ class GrafikController extends Controller
 {
     public function grafik()
     {
-        $products = Product::all();
+        $stores = Store::all();
         $x_y = [];
-        foreach ($products as $key => $product) {
-            $prevRate = $product->rates;
-            $prevTotal = 0;
-            for ($i = 0; $i < count($prevRate); $i++) {
-                $prevTotal += $prevRate[$i]->value;
+        foreach ($stores as $key => $store) {
+            if ($store->products) {
+                $products = $store->products;
+                $prevTotal = 0;
+                foreach ($products as $key => $product) {
+                    $prevRate = $product->rates;
+                    for ($i = 0; $i < count($prevRate); $i++) {
+                        $prevTotal += $prevRate[$i]->value;
+                    }
+                    // $prevAverage = $prevTotal / count($prevRate);
+                    // $user_click = floatval($product->rate - $prevAverage);
+                }
+                array_push($x_y, [$prevTotal,  $store->store_name]);
+            }else{
+                array_push($x_y, [0,  $store->store_name]);
             }
-            if (count($prevRate)>0) {
-                $prevAverage = $prevTotal / count($prevRate);
-                $user_click = floatval($product->rate - $prevAverage);
-                array_push($x_y, [$prevTotal,  $product->stores->store_name]);
-                # code...
-            }
+            # code...
+            // if (count($prevRate)>0) {
+            // }else{
+
+            // }
             // $updatedProduct = Product::find($id);
             // $rate_product = $updatedProduct->rates;
             // $total_rate = 0;
